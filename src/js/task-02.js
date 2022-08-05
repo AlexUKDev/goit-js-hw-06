5072bf9b6b47638750a0b5902e3acfe2b651e02d
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-08-05 14:05:40 +0300

    Script for Task 2
    Script for Task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,45 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients')
+// console.log(ulEl);
+
+const arrOfEl = [];
+
+ingredients.forEach((el) => {
+  // console.log(el)
+
+  const createEl = document.createElement('li');
+  createEl.textContent = el;
+  createEl.classList = 'item';
+  arrOfEl.push(createEl);
+  
+});
+
+// console.log(arrOfEl);
+
+ulEl.append(...arrOfEl)
+
+
+
+
+
+
+
+
+
+// const createEl = document.createElement('li')
+
+// createEl.textContent = ingredients[0]
+// console.log(createEl)
+
+
+
+
+// ***********
+
+// element.append(el1, el2, ...) - добавляет один или несколько элементов после всех детей элемента element.
+// element.prepend(el1, el2, ...) - добавляет один или несколько элементов перед всеми детьми элемента element.
+// element.after(el1, el2, ...) - добавляет один или несколько элементов после элемента element.
+// element.before(el1, el2, ...) - добавляет один или несколько элементов перед элементом element.
