7e938892eea157c033aa278185ca45cecb8d8f3b
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-08-06 17:36:27 +0300

    Script for Task 4
    Script for Task 4

---
+++ src/js/task-04.js
@@ -0,0 +1,63 @@
+
+
+const elRef = document.querySelector('#counter');
+const decrementBtnRef = elRef.querySelector('button[data-action="decrement"]')
+const incrementBtnRef = elRef.querySelector('button[data-action="increment"]')
+const valueRef = elRef.querySelector('#value')
+
+// console.log(elRef);
+
+
+decrementBtnRef.textContent = "Decrease";
+incrementBtnRef.textContent = 'Increase';
+
+decrementBtnRef.setAttribute("style", "font-size: 25px; padding: 10px; align-items: center;");
+incrementBtnRef.textContent = 'Increase';
+incrementBtnRef.setAttribute("style", "font-size: 25px; padding: 10px;");
+valueRef.setAttribute("style", "font-size: 50px; margin: 10px; ");
+
+// console.log(decrementBtnRef);
+// console.log(incrementBtnRef);
+// console.log(valueRef);
+
+// console.dir(valueRef);
+
+let counterValue = 0;
+
+// decrementBtnRef.addEventListener('click', function () {
+//   // console.log("CLIC on decrement");
+//   counterValue -= 1;
+//   valueRef.textContent = counterValue;
+// });
+
+// incrementBtnRef.addEventListener('click', function () {
+//   // console.log("CLIC on increment");
+//   counterValue += 1;
+//   valueRef.textContent = counterValue;
+// });
+
+const callBackIncrement = function () {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+};
+
+const callBackDecrement = () => {
+  counterValue -= 1;
+  valueRef.textContent = counterValue;
+};
+
+
+decrementBtnRef.addEventListener('click', callBackDecrement);
+incrementBtnRef.addEventListener('click', callBackIncrement);
+
+
+
+// const callBackIncrement = function () {
+//   counterValue += 1;
+//   valueRef.textContent = counterValue;
+// };
+
+// const callBackDecrement = () => {
+//   counterValue -= 1;
+//   valueRef.textContent = counterValue;
+// };
