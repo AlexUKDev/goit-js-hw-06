eb12fdd143346a522156a40d7c7376296c7cd23f
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-08-07 00:44:22 +0300

    Script for Task 9
    Script for Task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// console.log(getRandomHexColor())
+
+const changeColorBtnRef = document.querySelector("button.change-color ")
+console.log(changeColorBtnRef)
+
+const spanRef = document.querySelector("span.color")
+
+
+function onChangeColorBtnClick() {
+  
+  let newBodyBgColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newBodyBgColor;
+
+  spanRef.textContent = newBodyBgColor;
+}
+
+changeColorBtnRef.addEventListener('click', onChangeColorBtnClick);
